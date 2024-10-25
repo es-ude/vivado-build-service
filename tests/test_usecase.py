864515cc88e01a26d539a7ce6afd9f18c1a1086d
dominik-suk <dominik.sukic@web.de>
2024-10-25 13:24:35 +0200

    Update tests

--- tests/test_usecase.py
+++ tests/test_usecase.py
@@ -1,7 +1,10 @@
+import os
+import shutil
+import sys
 import threading
 import time
 
-from server import Server
+from buildserver import BuildServer
 from client import Client
 from src.config import ClientConfig, ServerConfig
 
@@ -9,6 +12,9 @@ from unittest import TestCase
 
 
 class Test(TestCase):
+    download_directory = 'tests/download'
+    testing_environment = ['tmp/server/Test', 'tmp/client/Test']
+
     server_vivado_user = 'dominik'
     server_port = 2025
 
@@ -29,36 +35,102 @@ class Test(TestCase):
         send_dir='tmp/client',
     )
 
-    def test(self):
-        server = Server(self.server_config)
-        server.general_config.is_test = True
+    server = BuildServer(server_config)
+    server.general_config.is_test = True
 
-        client = Client(self.client_config)
-        client.general_config.is_test = True
+    client = Client(client_config)
+    client.general_config.is_test = True
 
-        def threaded_server(s: Server):
+    def test_usage(self):
+        reset_testing_environment(self.testing_environment, self.download_directory)
+
+        def threaded_server(s: BuildServer):
             s.start()
 
         def threaded_client(c: Client):
             c.build(
                 upload_dir='tests/build_dir',
-                download_dir='tests/download',
+                download_dir=self.download_directory,
                 only_bin_files=True
             )
 
-        server_thread = threading.Thread(target=threaded_server, args=(server,))
-        client_thread = threading.Thread(target=threaded_client, args=(client,))
+        self.server_thread = threading.Thread(target=threaded_server, args=(self.server,))
+        self.client_thread = threading.Thread(target=threaded_client, args=(self.client,))
 
-        server_thread.start()
-        time.sleep(5)
-        client_thread.start()
+        # try
+        self.server_thread.start()
+        self.client_thread.start()
 
-        while(True):
-            ...
+        wait_until_files_are_downloaded(self.download_directory, timeout_in_seconds=10)
 
-    def TearDown(self):
-        ...
+        task_directory_exists = self.client.task_dir is not None
+        self.assertTrue(task_directory_exists, "Task directory does not exist")
+
+        self.assertTrue(
+            expr=compare_directories(dir1=self.client.task_dir, dir2=self.download_directory),
+            msg="Files are not equal")
+
+        if self.server is not None:
+            self.server.stop()
+        reset_testing_environment(self.testing_environment, self.download_directory)
+
+"""
+        try:
+            self.server_thread.start()
+            self.client_thread.start()
+
+            wait_until_files_are_downloaded(self.download_directory, timeout_in_seconds=10)
+
+            task_directory_exists = self.client.task_dir is not None
+            self.assertTrue(task_directory_exists, "Task directory does not exist")
+
+            self.assertTrue(
+                expr=compare_directories(dir1=self.client.task_dir, dir2=self.download_directory),
+                msg="Files are not equal")
+
+        except TimeoutError:
+            self.fail("Timed out waiting for completion")
+        except Exception as e:
+            self.fail(f"Connection could not be established: {e}")
+        finally:
+            if self.server is not None:
+                self.server.stop()
+            reset_testing_environment(self.testing_environment, self.download_directory)
+"""
+
+
+def reset_testing_environment(testing_env: list[str], download_dir: str):
+    for folder in testing_env + [download_dir]:
+        if not os.path.exists(folder):
+            continue
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+    for directory in testing_env:
+        if os.path.exists(directory):
+            os.rmdir(directory)
 
 
 def compare_directories(dir1, dir2):
     ...
+    return True
+
+
+def wait_until_files_are_downloaded(download_directory, timeout_in_seconds: int = None):
+    start = time.time()
+    while True:
+        elapsed_seconds = time.time() - start
+        download_directory_is_empty = len(os.listdir(download_directory)) == 0
+        if timeout_in_seconds is not None and elapsed_seconds > timeout_in_seconds:
+            raise TimeoutError
+        elif download_directory_is_empty:
+            time.sleep(1)
+            continue
+        else:
+            return
