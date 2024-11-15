990b94dbef2d2d98a18ce14960f941d5b21933d6
dominik-suk <dominik.sukic@web.de>
2024-11-15 13:28:10 +0100

    refactor

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ import os
 
 bash_dir = 'scripts/bash'
 
+
+def grant_permissions(f):
+    current_permissions = os.stat(f).st_mode
+    new_permissions = current_permissions | 0o111
+    os.chmod(f, new_permissions)
+
+
 for root, dirs, files in os.walk(bash_dir):
     for file in files:
         filepath = os.path.join(root, file)
         if file.split('_')[-1] == 'dos.sh' or file == 'testing.sh':
             unix_file = os.path.join(bash_dir, "_".join(file.split('_')[:-1])) + '_unix.sh'
             dos2unix(filepath, unix_file)
+            grant_permissions(filepath)
 
-        current_permissions = os.stat(filepath).st_mode
-        new_permissions = current_permissions | 0o111
-        os.chmod(filepath, new_permissions)
 
 reset()
+
