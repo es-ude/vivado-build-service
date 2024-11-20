12707ddeed83d904cccacbcd5875d693617997d6
dominik-suk <dominik.sukic@web.de>
2024-11-20 13:32:37 +0100

    update bash scripts

--- scripts/bash/autobuild_unix.sh
+++ scripts/bash/autobuild_unix.sh
@@ -7,34 +7,34 @@
 # $6 only-bin-file: 'output' all-project-files: '*'
 
 # Clear auto_build folder
-rm -rf /home/"$1"/.autobuild/*
+rm -rf .autobuild/*
 
 # Make directories for Vivado
-mkdir /home/"$1"/.autobuild/input_srcs
-mkdir /home/"$1"/.autobuild/input_srcs/srcs
-mkdir /home/"$1"/.autobuild/input_srcs/constraints
-mkdir /home/"$1"/.autobuild/vivado_project
-mkdir /home/"$1"/.autobuild/output
-mkdir /home/"$1"/.autobuild/tcl_script
+mkdir .autobuild/input_srcs
+mkdir .autobuild/input_srcs/srcs
+mkdir .autobuild/input_srcs/constraints
+mkdir .autobuild/vivado_project
+mkdir .autobuild/output
+mkdir .autobuild/tcl_script
 
 # Copy constraints into auto_build folder
-cp "$5" /home/"$1"/.autobuild/input_srcs/constraints
+cp "$5" .autobuild/input_srcs/constraints
 
 # Copy build folder
-cp -r "$3" /home/"$1"/.autobuild/input_srcs/srcs
+cp -r "$3" .autobuild/input_srcs/srcs
 
 # Let Vivado run
 export XILINXD_LICENSE_FILE=/opt/flexlm/Xilinx.lic
 /tools/Xilinx/Vivado/2021.1/bin/vivado -mode tcl -source "$2" >> "$4/run.log" 2>&1
 
 # Copy *bin file to output folder
-cp /home/"$1"/.autobuild/vivado_project/project_1.runs/impl_1/*.bin /home/"$1"/.autobuild/output/
+cp .autobuild/vivado_project/project_1.runs/impl_1/*.bin .autobuild/output/
 
 # Copy script in folder
-cp /home/"$1"/.autobuild_script/create_project_full_run.tcl /home/"$1"/.autobuild/tcl_script/
+cp .autobuild_script/create_project_full_run.tcl .autobuild/tcl_script/
 
 # Copy output folder to result folder
-cp -r /home/"$1"/.autobuild/"$6" "$4"
+cp -r .autobuild/"$6" "$4"
 
 # Clear auto_build folder
-rm -rf /home/"$1"/.autobuild/*
+rm -rf .autobuild/*
