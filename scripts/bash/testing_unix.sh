24541566e1d61e2057fd302627fb49668168b431
dominik-suk <dominik.sukic@web.de>
2024-11-20 13:26:42 +0100

    auto generate unix version of testing script

---
+++ scripts/bash/testing_unix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# $1 Vivado User
+# $2 filepath to tcl script 
+# $3 task build folder
+# $4 result output folder
+# $5 filepath to constraints
+
+# Create output directory
+mkdir -p "$4"/output >> error.log
+
+# Create completed.bin file
+cat > "$4"/output/completed.bin
