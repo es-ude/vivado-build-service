ecc84db6284e2d95d6614638b68072ec81615ad4
dominik-suk <dominik.sukic@web.de>
2024-11-22 14:29:19 +0100

    Refactor

--- src/user_queue.py
+++ src/user_queue.py
@@ -42,11 +42,10 @@ class Task:
 
 class UserQueue:
     def __init__(self):
-        self.user_queues = {}  # Stores tasks for each user
-        self.next_users = list()  # Keeps track of the order in which users are processed
+        self.user_queues = {}
+        self.next_users = list()
 
     def enqueue_task(self, task: Task):
-        # We use the task's user as the key, not extracting from the path
         client_id = task.user
         if client_id not in self.next_users:
             self.next_users.append(client_id)
@@ -54,58 +53,20 @@ class UserQueue:
         if client_id not in self.user_queues:
             self.user_queues[client_id] = []
 
-        # Add the task to the user's queue
         self.user_queues[client_id].append(task)
 
     def dequeue_task(self) -> Task | None:
         if len(self.user_queues) == 0:
-            # If no tasks are available, wait for a while before checking again
             time.sleep(0.1)
             return None
 
-        # Pop the next user from the queue (FIFO order)
-        next_user = self.next_users.pop(0)
-
-        # Pop the first task from the user's queue
-        task = self.user_queues[next_user].pop(0)
-
-        # If the user's queue is empty, remove them from the main user queue
-        if len(self.user_queues[next_user]) == 0:
-            del self.user_queues[next_user]
-        else:
-            # Otherwise, re-add the user to the list to continue processing their tasks later
-            self.next_users.append(next_user)
-
-        return task
-
-
-"""
-class UserQueue:
-    def __init__(self):
-        self.user_queues = {}
-        self.next_users = list()
-
-    def enqueue_task(self, task):
-        client_id = task.split('/')[-2]
-        if client_id not in self.next_users:
-            self.next_users.append(client_id)
-
-        if client_id not in self.user_queues:
-            self.user_queues[client_id] = list()
-
-        self.user_queues[client_id].append(task)
-
-    def dequeue_task(self):
-        if len(self.user_queues) == 0:
-            time.sleep(0.1)
-            return
-
         next_user = self.next_users.pop(0)
 
         task = self.user_queues[next_user].pop(0)
+
         if len(self.user_queues[next_user]) == 0:
             del self.user_queues[next_user]
         else:
             self.next_users.append(next_user)
+
         return task
-"""
