712f1c7574bd1f402677dc1d923f29d470ff1753
dominik-suk <dominik.sukic@web.de>
2025-04-11 12:16:44 +0200

    bin dir fix

--- src/threaded_tcp_handler.py
+++ src/threaded_tcp_handler.py
@@ -38,7 +38,12 @@ class ThreadedTCPHandler(socketserver.BaseRequestHandler):
         # use task class:
         user_queue.enqueue_task(task)
 
-        await_task_completion(directory=result_directory + '/bin')
+        if only_bin:
+            bin_dir = ''
+        else:
+            bin_dir = '/bin'
+        await_task_completion(directory=result_directory + bin_dir)
+
         response = prepare_response(result_directory)
         self.send(self, response, server_config)
         self.request.close()
