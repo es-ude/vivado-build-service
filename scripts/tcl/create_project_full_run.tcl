5f7233bf41aeaed7ab454beb19c44345447aab54
dominik-suk <dominik.sukic@web.de>
2025-05-13 13:40:34 +0200

    Exit on any error tcl

--- scripts/tcl/create_project_full_run.tcl
+++ scripts/tcl/create_project_full_run.tcl
@@ -1,6 +1,4 @@
-# run.tcl V2
-
-# NOTE: typical usage would be "vivado -mode tcl -source run.tcl -part <part_number>"
+# run.tcl V3 - exits on any error
 
 # Function to handle errors and exit Vivado
 proc exit_on_error {errorMsg} {
@@ -8,33 +6,34 @@ proc exit_on_error {errorMsg} {
     exit 1
 }
 
-# Check if a custom part number is provided via system arguments
-if {[llength $::argv] > 0} {
-    set part_number [lindex $::argv 0]
-}
-
-# STEP#1: Setup design sources and constraints
-create_project project_1 /home/vivado/.autobuild/vivado_project -part $part_number -force
-add_files /home/vivado/.autobuild/input_srcs/srcs
-add_files -fileset constrs_1 -norecurse /home/vivado/.autobuild/input_srcs/constraints/env5_config.xdc
-update_compile_order -fileset sources_1
-
-
-# STEP#2: run synthesis
+# Top-level catch block
 if {[catch {
+
+    # Check if a custom part number is provided via system arguments
+    if {[llength $::argv] > 0} {
+        set part_number [lindex $::argv 0]
+    } else {
+        error "No part number provided. Usage: vivado -mode tcl -source run.tcl -part <part_number>"
+    }
+
+    # STEP#1: Setup design sources and constraints
+    create_project project_1 /home/vivado/.autobuild/vivado_project -part $part_number -force
+    add_files /home/vivado/.autobuild/input_srcs/srcs
+    add_files -fileset constrs_1 -norecurse /home/vivado/.autobuild/input_srcs/constraints/env5_config.xdc
+    update_compile_order -fileset sources_1
+
+    # STEP#2: Run synthesis
     launch_runs synth_1 -jobs 12
     wait_on_run synth_1
-} errorMsg]} {
-    exit_on_error $errorMsg
-}
 
-# STEP#3: run implementation
-if {[catch {
+    # STEP#3: Run implementation
     set_property STEPS.WRITE_BITSTREAM.ARGS.BIN_FILE true [get_runs impl_1]
     launch_runs impl_1 -to_step write_bitstream -jobs 12
     wait_on_run impl_1
+
 } errorMsg]} {
     exit_on_error $errorMsg
 }
 
+# Exit cleanly if everything succeeds
 exit
